5995bf63dfd17403d443b0719b0174c334481b9a
Suraj Kumar Jana <surajjana2@gmail.com>
2017-12-08 10:37:21 +0530

    events list updated

--- email_attach.py
+++ email_attach.py
@@ -6,41 +6,37 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 
-SENDER = "Open Data Hack <hi@opendatahack.org>"
+SENDER = "Opencube Labs <hello@ocl.space>"
 
 
 AWS_REGION = "us-east-1"
 
 
-SUBJECT = "[TEST] Test 1"
+SUBJECT = "[CERTIFICATE] Arduino : Hands-on Workshop by Opencube Labs on 3rd December, 2017"
 
-def send_cert_attach(email_id, name):
+def send_cert_attach(email_id, name, file_name):
 
     RECIPIENT = email_id
 
-    # ATTACHMENT = "./odh_cert/" + file_name + ".pdf"
-
-    ATTACHMENT1 = "./test_attach/intro.pdf"
-    ATTACHMENT2 = "./test_attach/ocl.pdf"
-    ATTACHMENT3 = "./test_attach/cdp.pdf"
+    ATTACHMENT = "./ocl_cert/" + file_name + ".pdf"
 
 
-    BODY_TEXT = "Hello " + name + "!,\r\nThank you for attending Introduction to Open Data & Data Science Learn-up by Open Data Hack on 19th November, 2017. Please find attached your certificate.\n\nFollow us on Twitter(twitter.com/_opendatahack), like us on Facebook(fb.com/opendatahack) to get updated about our future events.\n\nThank you,\n\nRegards,\nSuraj Kumar Jana\nGlobal community coordinator\nhttps://www.opendatahack.org"
+    BODY_TEXT = "Hello " + name + "!,\r\nThank you for attending Arduino : Hands-on Workshop by Opencube Labs on 3rd December, 2017. Please find attached your certificate.\n\nFollow us on Twitter(twitter.com/opencubelabs), like us on Facebook(fb.com/opencubelabs) to get updated about our future events.\n\nThank you,\n\nRegards,\nSuraj Kumar Jana\nFounder & CEO, Opencube Labs\nhttp://ocl.space"
 
     BODY_HTML = """\
     <html>
     <head></head>
     <body>
     <h1>Hello """ + name + """!</h1>
-    <p>Thank you for attending Introduction to Open Data & Data Science Learn-up by Open Data Hack on 19th November, 2017. Please find attached your certificate.</p>
-    <p>Follow us on Twitter(twitter.com/_opendatahack), like us on Facebook(fb.com/opendatahack) to get updated about our future events.</p>
+    <p>Thank you for attending Arduino : Hands-on Workshop by Opencube Labs on 3rd December, 2017. Please find attached your certificate.</p>
+    <p>Follow us on Twitter(twitter.com/opencubelabs), like us on Facebook(fb.com/opencubelabs) to get updated about our future events.</p>
     <br>
     <p>Thank you,</p>
     <br>
     <p>Regards,</p>
     <p>Suraj Kumar Jana</p>
-    <p>Global community coordinator</p>
-    <p>https://www.opendatahack.org</p>
+    <p>Founder & CEO, Opencube Labs</p>
+    <p>http://ocl.space</p>
     </body>
     </html>
     """
@@ -62,23 +58,15 @@ def send_cert_attach(email_id, name):
     msg_body.attach(textpart)
     msg_body.attach(htmlpart)
 
-    att1 = MIMEApplication(open(ATTACHMENT1, 'rb').read())
+    att = MIMEApplication(open(ATTACHMENT, 'rb').read())
 
-    att1.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT1))
+    att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
 
-    att2 = MIMEApplication(open(ATTACHMENT2, 'rb').read())
-
-    att2.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT2))
-
-    att3 = MIMEApplication(open(ATTACHMENT3, 'rb').read())
-
-    att3.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT3))
+    
 
     msg.attach(msg_body)
 
-    msg.attach(att1)
-    msg.attach(att2)
-    msg.attach(att3)
+    msg.attach(att)
 
     try:
         response = client.send_raw_email(
@@ -97,12 +85,13 @@ def send_cert_attach(email_id, name):
         print(response['ResponseMetadata']['RequestId'])
 
 
-# names = ['Ali Raza','Abu Bakr','VR Krishna Prasad Sajja','Somnath Mahato','Nithyashree K','Salman Shariff','Rohith Kumar P']
-# email_ids = ['sanadi.raza@gmail.com','abuhaider2011@gmail.com','rankyp@gmail.com','somnathmahato723@gmail.com','nithyavishwakarma.gkm@gmail.com','salmanshariff.salman@gmail.com','rohithkumar31@gmail.com']
-# file_names = ['DS191117001','DS191117002','DS191117003','DS191117004','DS191117005','DS191117006','DS191117007']
+names = ['Abdul Rehman Iftekhar','Mohit Kumar','Preeti N Kini','Vinutha MR','Manjunath .P','Amruth Raj S.A','Deekshith Gowda H.P']
+email_ids = ['abdria26@gmail.com','m_kumar@students.iitmandi.ac.in','preetikini653@gmail.com','vinutharpkv@gmail.com','manjup93@gmail.com','amruthrajsagara@yahoo.com','deekshith2211@gmail.com']
+file_names = ['AR031217001', 'AR031217002', 'AR031217003', 'AR031217004', 'AR031217005', 'AR031217006', 'AR031217007']
 
-# for i in range(0, len(names)):
-#     print i+1
-#     send_cert_attach(email_ids[i], names[i], file_names[i])
+for i in range(0, len(names)):
+    print i+1
+    send_cert_attach(email_ids[i], names[i], file_names[i])
 
-send_cert_attach('surajjana2@gmail.com', 'Suraj')
+
+# send_cert_attach('surajjana2@gmail.com', 'Suraj', 'AR031217001')
